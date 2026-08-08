b707ffed6bd40521ba409095f389337622ced78b
Kunal Bose <kunalbose.2525@gmail.com>
2026-08-08 12:43:24 +0530

    temp: enable error display to diagnose production issue

--- config/config.php
+++ config/config.php
@@ -64,6 +64,6 @@ if (IS_LOCAL) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 } else {
-    error_reporting(0);
-    ini_set('display_errors', 0);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 }
